9a3affc450c992dd3399d6a5e1c81c187c549632
jasonzhouu <jasonzhouu@outlook.com>
2021-06-04 14:50:47 +0800

    fix

--- contentScript.js
+++ contentScript.js
@@ -27,12 +27,12 @@ const filterButton = (cardListEle) => {
   const card = Array.from(cardListEle.querySelectorAll("li")).find((card) => {
     const button = card.querySelector("button.btn-greet");
     const sexIcon = card.querySelector("div.avatar-box i");
-    const desceription = card.querySelector("div.advantage-new");
+    const description = card.querySelector("div.advantage-new");
     return (
       button &&
       button.innerText === "打招呼" &&
       !sexIcon.className.endsWith("women") &&
-      desceription.toLowerCase().includes("react")
+      description.innerText.toLowerCase().includes("react")
     );
   });
   if (card) {
